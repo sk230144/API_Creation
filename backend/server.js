16ea4fb986c97c20d58299080120b2541915ddc7
Saurabh Tiwari <95127073+sk230144@users.noreply.github.com>
2024-03-20 06:21:59 -0400

    Update server.js
    Nothing

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors(
-  {
-     origin: ["https://api-creation.vercel.com"],
-    methods: ["POST", "GET", "PUT"],
-    credentials: true
-  }
-));
+app.use(cors());
 app.use(express.json())
 
 // Connect to MongoDB
